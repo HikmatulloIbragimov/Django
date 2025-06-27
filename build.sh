27a50d139f4cab34b218a3bb77143eaa620df105
Ohayo <lakdown373@gmail.com>
2025-06-27 11:17:15 +0500

    sdfsd

--- build.sh
+++ build.sh
@@ -1,15 +1,18 @@
-echo '#!/bin/sh' > /home/render/colors.sh || true
-echo 'exit 0' >> /home/render/colors.sh || true
 #!/usr/bin/env bash
 set -eo pipefail
 
-# ✅ Ставим ffmpeg в свою папку
+# ⚠️ Fix Render's tput bug
+if [ -f /home/render/colors.sh ]; then
+  sed -i 's/tput.*/true/' /home/render/colors.sh
+fi
+
+# Установка ffmpeg
 mkdir -p bin
 curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz | tar -xJ
 mv ffmpeg-*-static/ffmpeg ./bin/ffmpeg
 mv ffmpeg-*-static/ffprobe ./bin/ffprobe
 chmod +x ./bin/ffmpeg ./bin/ffprobe
 
-# ✅ Добавляем ffmpeg в PATH
+# Добавление ffmpeg в PATH
 echo 'export PATH="$(pwd)/bin:$PATH"' >> ~/.profile
 export PATH="$(pwd)/bin:$PATH"
