2f738e3e1f22ff629f87998e8eac23420add7609
Ohayo <lakdown373@gmail.com>
2025-06-27 11:15:07 +0500

    sdfs

--- build.sh
+++ build.sh
@@ -1,21 +1,15 @@
+echo '#!/bin/sh' > /home/render/colors.sh || true
+echo 'exit 0' >> /home/render/colors.sh || true
 #!/usr/bin/env bash
-
-
-echo "Disabling tput error..."
-touch /home/render/colors.sh
-echo '#!/bin/sh' > /home/render/colors.sh
-echo 'exit 0' >> /home/render/colors.sh
-
 set -eo pipefail
-tput setaf 2 || true
-apt update && apt install -y ncurses-bin
 
-# Установка ffmpeg для moviepy
+# ✅ Ставим ffmpeg в свою папку
 mkdir -p bin
 curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz | tar -xJ
 mv ffmpeg-*-static/ffmpeg ./bin/ffmpeg
 mv ffmpeg-*-static/ffprobe ./bin/ffprobe
 chmod +x ./bin/ffmpeg ./bin/ffprobe
 
-# Обновление PATH
+# ✅ Добавляем ffmpeg в PATH
+echo 'export PATH="$(pwd)/bin:$PATH"' >> ~/.profile
 export PATH="$(pwd)/bin:$PATH"
