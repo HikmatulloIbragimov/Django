3bb56b71926a3ef45939ede8094a4be29ee4e6e5
Ohayo <lakdown373@gmail.com>
2025-06-27 11:38:30 +0500

    sdfsd

--- build.sh
+++ build.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 set -e
-
-# Установка ffmpeg
 mkdir -p bin
 curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz | tar -xJ
 mv ffmpeg-*-static/ffmpeg ./bin/ffmpeg
 mv ffmpeg-*-static/ffprobe ./bin/ffprobe
 chmod +x ./bin/ffmpeg ./bin/ffprobe
-
-# Добавим ffmpeg в PATH для дальнейших команд
 export PATH="$(pwd)/bin:$PATH"
